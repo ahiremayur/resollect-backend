f407ca103e454b2bf31d54d298decc26143adafb
mayurahire <mayurahire67@gmail.com>
2025-05-14 20:43:46 +0530

    checking deployment

--- smarttodo/settings.py
+++ smarttodo/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+WSGI_APPLICATION = 'api.wsgi.app'
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
